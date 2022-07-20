574b799d87608a2129c5db9d420286e642925516
sarahemaginnis <sarahemaginnis@gmail.com>
2022-07-19 19:56:14 -0500

    Add project files.

---
+++ ClassesPractice2/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesPractice2
+{
+    internal class Person
+    {
+        public Person(string name, string favoriteColor, string favoriteFood)
+        {
+            Name = name;
+            FavoriteColor = favoriteColor;
+            FavoriteFood = favoriteFood;
+        }
+
+        public string Name { get; set; }
+        public string FavoriteColor { get; set; }
+
+        public string FavoriteFood { get; set; }
+
+        //The following DisplayInfo might be a violation of the Single Principle
+        //This deals with UI and typically UI has a separate class
+        internal void GetDisplayInfo()
+        {
+            string info = @$"
+Name: {Name}
+Favorite Color: {FavoriteColor}
+Favorite Food: {FavoriteFood}
+";
+            Console.WriteLine(info);
+        }
+    }
+}
---
+++ ClassesPractice2/Program.cs
@@ -0,0 +1,43 @@
+﻿using ClassesPractice2;
+
+//SOLID Principles 
+// S - Single Responsibility Principle
+//O - Open/Closed Principle - Classes should be open to extension and closed to modification
+
+List<Person> people = new List<Person>();
+
+while (true)
+{
+    //ask for name
+    Console.Write("Please enter your name: (type 'quit' to quit): ");
+
+    string nameInput = Console.ReadLine();
+    if (nameInput.ToLower() == "quit")
+    {
+        break;
+    }
+
+    //ask for favorite color
+    Console.WriteLine(); //blank writeline for some spacing on the console screen
+    Console.Write("What is your favorite color? ");
+
+    string colorInput = Console.ReadLine();
+
+    //favorite food
+    Console.WriteLine();
+    Console.Write("What is your favorite food? ");
+
+    string foodInput = Console.ReadLine();
+
+    //capture entries into object
+    Person person = new Person(nameInput, colorInput, foodInput);
+    people.Add(person);
+
+}
+foreach (Person person in people)
+{
+    //summarize all info entered so far
+    person.GetDisplayInfo();
+
+}
+Console.ReadLine();
